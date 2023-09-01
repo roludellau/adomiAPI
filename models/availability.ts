3516fb04772e6a666c03e2ad817ddf2140dd0ac8
TheoSoko <theo.sokolowsky@gmail.com>
2023-09-01 02:47:19 +0200

    added validations for carer methods

--- models/availability.ts
+++ models/availability.ts
@@ -1,37 +1,58 @@
 'use strict';
-import {
-  HasMany,
-  Model
-} from 'sequelize';
+import { HasMany, Model } from 'sequelize';
 
 interface availabilitiesAttributes{
   week_day:number,
   start_hour:Date,
   end_hour:Date
 }
+
 module.exports = (sequelize: any, DataTypes: { NUMBER: any; TIME: any; }) => {
-  class Availability extends Model<availabilitiesAttributes>
-  implements availabilitiesAttributes{
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    class Availability extends Model<availabilitiesAttributes> implements availabilitiesAttributes{
+        week_day!: number;
+        start_hour!:Date;
+        end_hour!:Date;
+    }
 
-    week_day!: number;
-    start_hour!:Date;
-    end_hour!:Date;
+    Availability.init(
+      {
+          week_day: {
+              type: DataTypes.NUMBER,
+              allowNull: false,
+              validate: {
+                  notNull: { msg: "Le jour de la semaine doit être indiqué" },
+                  isNumeric: { msg: "Veuillez entrer une valeur numérique pour le jour de la semaine (1 à 7)" },
+              }
+          },
+          start_hour: {
+              type: DataTypes.TIME,
+              allowNull: false,
+              validate: {
+                  notNull: { msg: "L'heure de début doit être indiquée" },
+                  is: {
+                      args: /^(?:[01]\d|2[0-3]):(?:[0-5]\d)(:[0-5]\d){0,1}$/,
+                      msg: "L'heure de début doit être une date valide (hh:mm:hh ou hh:mm)" 
+                  },
+              }
+          },
+          end_hour: {
+              type: DataTypes.TIME,
+              allowNull: false,
+              validate: {
+                  notNull: { msg: "L'heure de fin doit être indiquée" },
+                  is: {
+                      args: /^(?:[01]\d|2[0-3]):(?:[0-5]\d)(:[0-5]\d){0,1}$/,
+                      msg: "L'heure de fin doit être une date valide (hh:mm:hh ou hh:mm)" 
+                },
+            }
+          }
+      }, 
+      {
+        sequelize,
+        modelName: 'Availability',
+        tableName: 'availabilities'
+      }
+    )
 
-
-  }
-  Availability.init({
-    week_day: DataTypes.NUMBER,
-    start_hour: DataTypes.TIME,
-    end_hour: DataTypes.TIME
-  }, {
-    sequelize,
-    modelName: 'Availability',
-    tableName: 'availabilities'
-  });
-  return Availability;
+    return Availability;
 };
